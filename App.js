f2510a81ba76686a020a13bf24a494679ccc6a73
rameshparajuli <rameshparajuli09@gmail.com>
2020-06-27 09:37:29 +0545

    'request_demo'

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ const serverMessage = () => {
 require("http")
   .createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Ramesh Parajuli");
+    // res.statusCode = 200;
+    // res.setHeader("Content-Type", "text/plain");
+    // res.end("Ramesh Parajuli");
     process.exit();
-    // if you want to exit
+    // // if you want to exit
   })
   .listen(3000, "localhost", serverMessage());
